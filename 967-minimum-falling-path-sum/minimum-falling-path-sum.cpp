e54c3fe202c706c12d65c5405903f7e2397a8065
Anva41103 <146993960+Anva41103@users.noreply.github.com>
2024-02-11 02:07:37 +0530

    Time: 11 ms (67.36%) | Memory: 13.5 MB (6.83%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    // int teddy(vector < vector<int>> & dp, vector<vector<int>>& m, int i, int j) {
-    //     if (j < 0 || j >= m[0].size())
-    //         return 1e9;
-    //     if (i == 0)
-    //         return dp[i][j] = m[i][j];
-        
-    //     if (dp[i][j] != -1)
-    //         return dp[i][j];
-    //     int u = m[i][j] + teddy(dp, m, i - 1, j);
-    //     int rd = m[i][j] + teddy(dp, m, i - 1, j + 1);
-    //     int ld = m[i][j] + teddy(dp, m, i - 1, j - 1);
-    //     return dp[i][j] = min(u, min(ld, rd));
-    // }
+    int f(int row,int col,int n,vector<vector<int>>& arr,vector<vector<int>>& dp){
+        if(row<0||row==n||col<0||col==n) return 1e9;
+        if(row==n-1) return arr[row][col];
+        if(dp[row][col]!=101) return dp[row][col];
+        return dp[row][col]=arr[row][col]+min(f(row+1,col,n,arr,dp),min(f(row+1,col+1,n,arr,dp),f(row+1,col-1,n,arr,dp)));
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        int mini = INT_MAX;
-        for(int j=0;j<n;j++)
-        {
-            dp[0][j]=matrix[0][j];
-        }
-        for(int i=1;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                int u = matrix[i][j] + dp[i - 1][j],rd=1e9,ld=1e9;
-                if(j+1<n) rd = matrix[i][j] + dp[i - 1][j+1];
-                if(j-1>=0) ld = matrix[i][j] +dp[i - 1][j-1];;
-                dp[i][j] = min(u, min(ld, rd));
-            }
-        }
-        for(int j=0;j<n;j++)
-        {
-            mini=min(mini,dp[m-1][j]);
+        int n=matrix.size();
+        vector<vector<int>> dp(n,vector<int>(n,101));
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++){
+            int ans=f(0,i,n,matrix,dp);
+            mini=min(mini,ans);
         }
         return mini;
     }
